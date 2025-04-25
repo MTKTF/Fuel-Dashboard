e4ec34cedcd7b7d4083042b86ddc780739d44ccd
MTKTF <31927665+MTKTF@users.noreply.github.com>
2025-04-25 14:35:06 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,11 @@
+const dummyData = [
+    { keyfob_id: 1, fuel_pumped: 20, tank_level: 46500, timestamp: "2025-04-18 10:00" },
+    { keyfob_id: 2, fuel_pumped: 15, tank_level: 46485, timestamp: "2025-04-18 12:00" },
+    { keyfob_id: 3, fuel_pumped: 30, tank_level: 46455, timestamp: "2025-04-19 14:00" },
+    { keyfob_id: 1, fuel_pumped: 25, tank_level: 46430, timestamp: "2025-04-20 16:00" },
+    { keyfob_id: 2, fuel_pumped: 18, tank_level: 46412, timestamp: "2025-04-21 09:00" }
+];
+
+function getDummyData() {
+    return dummyData;
+}
